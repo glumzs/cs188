8456f8948a83eb18455206e688d81673436cd12f
Plamen Levchev <plamen@berkeley.edu>
2012-06-18 17:45:53 -0700

    added generic utils

---
+++ js/utils.js
@@ -0,0 +1,9 @@
+function hexc(colorval) {
+    var parts = colorval.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    delete(parts[0]);
+    for (var i = 1; i <= 3; ++i) {
+        parts[i] = parseInt(parts[i]).toString(16);
+        if (parts[i].length == 1) parts[i] = '0' + parts[i];
+    }
+    color = '#' + parts.join('');
+}
